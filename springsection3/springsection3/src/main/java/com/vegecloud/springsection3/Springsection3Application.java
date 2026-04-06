0ed7a4e35c3c01f92aa504f680d033470e6e565f
vegecloud <caiyun_yap@hotmail.com>
2026-04-07 00:02:36 +0800

    chore: update files

---
+++ springsection3/springsection3/src/main/java/com/vegecloud/springsection3/Springsection3Application.java
@@ -0,0 +1,13 @@
+package com.vegecloud.springsection3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springsection3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springsection3Application.class, args);
+	}
+
+}
---
+++ springsection3/springsection3/src/test/java/com/vegecloud/springsection3/Springsection3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vegecloud.springsection3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springsection3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
